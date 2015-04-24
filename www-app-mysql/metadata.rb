aa89618e80de3db0aa48b6bfb388259ddb92e608
Joel Scheuner <joel.scheuner.dev@gmail.com>
2015-04-24 17:15:01 +0200

    Add Berkshelf support and fix dependencies for backwards compatibility

--- www-app-mysql/metadata.rb
+++ www-app-mysql/metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends 'apt'
-depends 'openssl'
-depends 'build-essential'
-depends 'mysql'
-depends 'database'
+depends 'openssl', '1.1.0'
+depends 'build-essential', '2.0.2'
+depends 'mysql', '5.2.6'
+depends 'database', '2.2.0'
