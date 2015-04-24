0940f6708f93a9efb0f8d03d3d7d99e38a5125b6
Joel Scheuner <joel.scheuner.dev@gmail.com>
2015-04-24 18:17:39 +0200

    Import sysbench and fio-benchmark

---
+++ sysbench/attributes/default.rb
@@ -0,0 +1,2 @@
+default['sysbench']['cli_options']['test'] = 'cpu'
+default['sysbench']['cli_options']['num-threads'] = 1
---
+++ sysbench/recipes/default.rb
@@ -0,0 +1,2 @@
+include_recipe 'sysbench::install'
+include_recipe 'sysbench::configure'
