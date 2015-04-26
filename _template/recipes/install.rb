81b50e3ddd51fb9d8948ef3b64a24269afbf3227
Joel Scheuner <joel.scheuner.dev@gmail.com>
2015-04-26 13:15:35 +0200

    Clarify comment for example install recipe

--- _template/recipes/install.rb
+++ _template/recipes/install.rb
@@ -1,13 +1,10 @@
-# Install your benchmark here by leveraging
+### Install your benchmark here by leveraging
 # Chef resources: http://docs.chef.io/resources.html
 # Community cookbooks: https://supermarket.chef.io/dashboard
 
-# Example:
-# Update apt package index
+## Example:
+# Update apt package index (make sure you declare dependencies in metadata.rb)
 # include_recipe 'apt'
-# => You'll need to declare external dependencies in
-# metadata.rb such as:
-# depends 'apt', '~> 2.7.0'
 
 # package 'YOUR_PACKAGE' do
 #   action :install
