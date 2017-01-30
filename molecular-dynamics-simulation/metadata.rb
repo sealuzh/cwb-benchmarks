d2f4829199be7cb4fb8afbe83a8bf6c211ae6dab
Joel Scheuner <joel.scheuner.dev@gmail.com>
2017-01-30 07:41:35 +0100

    Update dependencies

--- molecular-dynamics-simulation/metadata.rb
+++ molecular-dynamics-simulation/metadata.rb
@@ -7,6 +7,6 @@ description      'Installs/Configures molecular-dynamics-simulation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'cwb', '~> 0.1.0'
-depends 'apt', '~> 2.7.0'
-depends 'build-essential', '~> 2.2.3'
+depends 'cwb', '~> 0.1.3'
+depends 'apt', '~> 5.0.1'
+depends 'build-essential', '~> 7.0.3'
