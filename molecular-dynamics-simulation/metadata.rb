eca61f439e97808d0a14901abaa1b6e73e1a98a3
Joel Scheuner <joel.scheuner.dev@gmail.com>
2017-01-30 07:42:19 +0100

    Bump version of molecular dynamics simulation to 0.1.1

--- molecular-dynamics-simulation/metadata.rb
+++ molecular-dynamics-simulation/metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'joel.scheuner.dev@gmail.com'
 license          'MIT'
 description      'Installs/Configures molecular-dynamics-simulation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'cwb', '~> 0.1.3'
 depends 'apt', '~> 5.0.1'
