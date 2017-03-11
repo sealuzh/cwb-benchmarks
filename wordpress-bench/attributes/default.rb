8316672e501e34724daafce2b52baa8d3badb205
Joel Scheuner <joel.scheuner.dev@gmail.com>
2017-03-11 21:14:41 +0100

    Set timeout defaults in `wordpress-bench`

--- wordpress-bench/attributes/default.rb
+++ wordpress-bench/attributes/default.rb
@@ -12,11 +12,18 @@ default['wordpress-bench']['load_generator'] = 'http://localhost'
 # Cookbook wherein the `test_plan.jmx` resides
 default['wordpress-bench']['test_plan_cookbook'] = 'wordpress-bench'
 
+## JMeter properties
+# Site URL
 http_site = node['wordpress']['site']['url']
-# JMeter properties
 default['wordpress-bench']['jmeter']['properties']['site'] = (http_site.sub('http://', '') rescue '127.0.0.1')
 # Thread-group specific
 default['wordpress-bench']['jmeter']['properties']['target_concurrency'] = 80 # i.e., users
 default['wordpress-bench']['jmeter']['properties']['ramp_up_time'] = 5 # time while the users are gradually increased
 default['wordpress-bench']['jmeter']['properties']['ramp_up_steps_count'] = 7 # number of steps from 0 to [target_concurrency]
 default['wordpress-bench']['jmeter']['properties']['hold_target_rate_time'] = 2 # time to hold the target load
+# Data export
+# default['wordpress-bench']['jmeter']['properties']['jmeter.save.saveservice.print_field_names'] = true
+# default['wordpress-bench']['jmeter']['properties']['jmeter.save.saveservice.output_format'] = 'csv'
+# HTTP defaults
+default['wordpress-bench']['jmeter']['properties']['connect_timeout'] = 10000
+default['wordpress-bench']['jmeter']['properties']['response_timeout'] = 40000
