94516fefcfb7015bd1a25694f35749880fc99b19
Joel Scheuner <joel.scheuner.dev@gmail.com>
2017-03-14 16:38:43 +0100

    Bump `wordpress-bench` to support distributed testing

--- wordpress-bench-test-plan/metadata.rb
+++ wordpress-bench-test-plan/metadata.rb
@@ -8,4 +8,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.2'
 
 depends 'cwb', '~> 0.1.0'
-depends 'wordpress-bench', '~> 2.0.3'
+depends 'wordpress-bench', '~> 2.0.4'
