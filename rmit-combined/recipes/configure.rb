08a2f2a204f477983b6768a9a461e7ef4a49c7d3
Joel Scheuner <joel.scheuner.dev@gmail.com>
2017-03-30 01:07:23 +0200

    Add all benchmarks to the list again

--- rmit-combined/recipes/configure.rb
+++ rmit-combined/recipes/configure.rb
@@ -1,17 +1,17 @@
 cwb_benchmark_suite 'rmit-benchmark-suite'
 
 # Implementations provided under `files/default/benchmark_name.rb`
-# cwb_benchmark 'sysbench-cpu'
-# cwb_benchmark 'sysbench-memory'
-# cwb_benchmark 'sysbench-threads'
-# cwb_benchmark 'sysbench-mutex'
-# cwb_benchmark 'sysbench-fileio'
-# cwb_benchmark 'fio'
-# cwb_benchmark 'stressng-cpu'
-# cwb_benchmark 'stressng-network'
-# cwb_benchmark 'iperf'
-# include_recipe 'rmit-combined::configure_remote_client'
-# cwb_benchmark 'md-sim'
+cwb_benchmark 'sysbench-cpu'
+cwb_benchmark 'sysbench-memory'
+cwb_benchmark 'sysbench-threads'
+cwb_benchmark 'sysbench-mutex'
+cwb_benchmark 'sysbench-fileio'
+cwb_benchmark 'fio'
+cwb_benchmark 'stressng-cpu'
+cwb_benchmark 'stressng-network'
+cwb_benchmark 'iperf'
+include_recipe 'rmit-combined::configure_remote_client'
+cwb_benchmark 'md-sim'
 
 ### Wordpress Benchmark
 def config_jmeter(properties)
