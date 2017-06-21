8d3922f980a4f3f3ca5606ead586faa0b8ad3637
Philipp Leitner <leitner@infosys.tuwien.ac.at>
2017-06-21 12:13:05 +0200

    Updated version nr

--- software-microbenchmarking-runner/metadata.rb
+++ software-microbenchmarking-runner/metadata.rb
@@ -5,7 +5,7 @@ name             'software-microbenchmarking-runner'
 license          'MIT'
 description      'Installs/Configures software-microbenchmarking-runner'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 depends 'cwb', '~> 0.1.0'
 depends 'apt', '~> 2.7.0'
