e81d9af4cc17bb25417626a8d7f20d77a43bc7ed
Philipp Leitner <leitner@infosys.tuwien.ac.at>
2017-06-22 16:35:18 +0200

    First attempt to do clone and compilation as part of the Chef run

--- jmh-runner/recipes/install.rb
+++ jmh-runner/recipes/install.rb
@@ -9,3 +9,49 @@ package 'default-jdk'
 package 'maven'
 package 'git'
 gem_package 'git'
+
+projects = node['jmh-runner']['projects']
+puts "Starting to install Java projects to benchmark"
+projects.each do |project|
+
+  # clone and checkout repo
+  group = project['project']['github']['group']
+  name = project['project']['github']['name']
+  url = "https://github.com/%s/%s.git" % [group, name]
+
+  puts "Installing #{url}"
+
+  bash "clone_project" do
+   cwd '../files/default'
+   code "git clone #{url}"
+   version = project['project']['version']
+   if version
+     cwd name
+     code "git checkout #{version}"
+   end
+   puts "Successfully cloned and checked out project"
+   action :nothing
+  end
+
+  # compile using the appropriate backend
+  backend = project['project']['backend']
+  case backend
+    when 'gradle'
+      target = project['project']['gradle']['build_cmd']
+      bash "compile_project" do
+       cwd name
+       code "./gradlew #{target}"
+       action :nothing
+      end
+    when 'mvn'
+      bash "compile_project" do
+       cwd name
+       code 'mvn clean install -DskipTests'
+       action :nothing
+      end
+    else
+      raise "Unsupported backend " + backend
+  end
+  puts "Successfully compiled project"
+
+end
