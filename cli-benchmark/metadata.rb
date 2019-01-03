3d2db4333f5f32c38aa7c16461e5bf975a3d5afb
Joel Scheuner <joel.scheuner.dev@gmail.com>
2019-01-03 18:31:38 +0100

    Fix foodcritic metadata issues

--- cli-benchmark/metadata.rb
+++ cli-benchmark/metadata.rb
@@ -1,10 +1,15 @@
 name             'cli-benchmark'
 maintainer       'Joel Scheuner'
 maintainer_email 'joel.scheuner.dev@gmail.com'
+source_url       'https://github.com/sealuzh/cwb-benchmarks'
+issues_url       'https://github.com/sealuzh/cwb-benchmarks/issues'
 license          'MIT'
 description      'Installs/Configures cli-benchmark'
 long_description 'Installs/Configures cli-benchmark'
 version          '1.1.0'
+
+supports 'ubuntu'
+chef_version '~> 12'
 
 depends 'cwb'
 depends 'apt'
